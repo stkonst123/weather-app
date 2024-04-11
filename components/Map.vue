7029bfeaff0f9488826b03c9b6f5e0a25de4615e
Konstantin Stepanov <stkonst123@yandex.ru>
2024-04-11 22:26:53 +0900

    Location pointer implemented

--- components/Map.vue
+++ components/Map.vue
@@ -1,18 +1,23 @@
 <script setup lang="ts">
 const latitude = useState("latitude");
 const longitude = useState("longitude");
+
 const projection = ref("EPSG:4326");
 const zoom = ref(4);
 const rotation = ref(0);
 </script>
 
 <template>
-  <ol-map style="height: 100%">
+  <ol-map style="height: 100%" :controls="[]">
     <ol-view ref="view" :center="[longitude, latitude]" :zoom="zoom" :projection="projection" :rotation="rotation" />
 
     <ol-tile-layer>
       <ol-source-osm />
     </ol-tile-layer>
+
+    <ol-overlay :position="[longitude, latitude]">
+      <div class="overlay-content"></div>
+    </ol-overlay>
   </ol-map>
 </template>
 
@@ -31,8 +36,8 @@ const rotation = ref(0);
   margin-top: -40px;
   margin-left: -40px;
   border-radius: 50%;
-  border: 5px solid rgba(180, 180, 180, 0.6);
-  border-top-color: var(--vp-c-brand-1);
+  border: 5px solid dodgerblue;
+  border-top-color: orange;
   animation: spinner 0.6s linear infinite;
 }
 
@@ -40,5 +45,14 @@ const rotation = ref(0);
   to {
     transform: rotate(360deg);
   }
+}
+
+.overlay-content {
+  width: 30px;
+  height: 30px;
+  translate: -15px -15px;
+  border-radius: 50%;
+  background-color: orange;
+  box-shadow: 0 0 5px 1px black;
 }
 </style>
